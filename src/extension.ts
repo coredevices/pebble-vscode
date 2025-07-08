1261f81376c4e9244770925c5baf9fd273738932
Griffin Li <griffin@griffinli.com>
2025-07-08 14:17:21 -0400

    Add project creation command

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from 'child_process';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,10 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('pebble-vscode.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('pebble-vscode.newProject', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from pebble-vscode!');
+		// vscode.window.showInformationMessage('Hello World from pebble-vscode!');
+		createProject();
 	});
 
 	context.subscriptions.push(disposable);
@@ -24,3 +26,49 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function createProject() {
+	const projectType = await vscode.window.showQuickPick(['C (default)', 'C basic', 'C and JS'], {
+	});
+
+	const projectName = await vscode.window.showInputBox({
+		prompt: 'Enter the name of the new project',
+	});
+
+	const folderUri = await vscode.window.showOpenDialog({
+		canSelectFiles: false,
+		canSelectFolders: true,
+		canSelectMany: false,
+		openLabel: 'Select a folder to create the project in',
+		title: 'Create a new Pebble project',
+	});
+
+	if (!folderUri || folderUri.length === 0) {
+		vscode.window.showErrorMessage('No folder selected. Project creation cancelled.');
+		return;
+	}
+
+	const projectPath = folderUri[0].fsPath;
+
+	let command = 'pebble new-project --c';
+	if (projectType === 'C simple') {
+		command += ' --simple';
+	} else if (projectType === 'C and JS') {
+		command += ' --javascript';
+	}
+
+	cp.exec(`${command} ${projectName}`, {
+		cwd: projectPath
+	}, (error, stdout, stderr) => {
+		if (error) {
+			vscode.window.showErrorMessage(`Error creating project: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			vscode.window.showErrorMessage(`Error: ${stderr}`);
+			return;
+		}
+		vscode.window.showInformationMessage(`Project created successfully: ${stdout}`);
+		vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(`${projectPath}/${projectName}`));
+	});
+}

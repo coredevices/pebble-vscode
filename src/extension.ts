9a5a558e5cfb851dbb922c586f4e228776fb815a
Griffin Li <griffin@griffinli.com>
2025-07-09 17:31:59 -0400

    Add install on phone

--- src/extension.ts
+++ src/extension.ts
@@ -33,7 +33,15 @@ export async function activate(context: vscode.ExtensionContext) {
 		runWithArgs('--logs');
 	});
 
-	vscode.commands.registerCommand('pebble-vscode.setDefaultPlatform', async () => {
+	const runOnPhone = vscode.commands.registerCommand('pebble-vscode.runOnPhone', async () => {
+		runOnPhoneWithArgs();
+	});
+
+	const runOnPhoneWithLogs = vscode.commands.registerCommand('pebble-vscode.runOnPhoneWithLogs', async () => {
+		runOnPhoneWithArgs('--logs');
+	});
+
+	const setDefaultPlatform = vscode.commands.registerCommand('pebble-vscode.setDefaultPlatform', async () => {
 		const platform = await requestEmulatorPlatform();
 		if (!platform) {
 			vscode.window.showErrorMessage('No platform selected. Action cancelled.');
@@ -53,10 +61,57 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	// context.subscriptions.push(vscode.window.registerTreeDataProvider('myTreeView', new PebbleTreeProvider()));
 
-	context.subscriptions.push(newProject, run, runWithLogs);
+	context.subscriptions.push(newProject, run, runWithLogs, setDefaultPlatform, runOnPhone, runOnPhoneWithLogs);
 }
 
 export function deactivate() {}
+
+async function runOnPhoneWithArgs(args = '') {
+	const workspacePath = getWorkspacePath();
+	if (!workspacePath) {
+		vscode.window.showErrorMessage('No workspace folder is open. Please open a workspace folder to run the project.');
+		return;
+	}
+
+	const phoneIp = await getPhoneIp();
+	if (!phoneIp) {
+		vscode.window.showErrorMessage('No phone IP address provided. Installation cancelled.');
+		return;
+	}
+
+	let terminal = vscode.window.terminals.find(t => t.name === `Pebble Run`);
+	if (!terminal) {
+		terminal = vscode.window.createTerminal(`Pebble Run`);
+	}
+
+	terminal.show();
+	terminal.sendText('\x03'); // Send Ctrl+C
+	terminal.sendText(`pebble build && pebble install --phone ${phoneIp}${args ? ' ' + args : ''}`, true);
+}
+
+async function getPhoneIp() {
+
+	const storedPhoneIp = vscode.workspace.getConfiguration('pebble-vscode').get<string>('phoneIp');
+	if (storedPhoneIp) {
+		return storedPhoneIp;
+	}
+
+	const phoneIp = await vscode.window.showInputBox({
+		prompt: 'Enter the IP address of phone. Find it in developer settings on your Pebble app.'
+	});
+
+	const setDefault = await vscode.window.showQuickPick(['No', 'Yes'], {
+		placeHolder: 'Set this IP as the default for future runs?',
+		canPickMany: false,
+	});
+
+	if (setDefault === 'Yes') {
+		const config = vscode.workspace.getConfiguration('pebble-vscode');
+		await config.update('phoneIp', phoneIp, vscode.ConfigurationTarget.Global);
+	}
+
+	return phoneIp;
+};		
 
 async function runWithArgs(args = '') {
 	const platform = await getEmulatorPlatform();
